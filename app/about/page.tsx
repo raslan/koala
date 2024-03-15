f730db190966547c059b53794a962c43dc6dc351
raslan <24810123+raslan@users.noreply.github.com>
2024-03-15 19:55:48 +0200

    feat: add app icon and name

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div>
+      <footer className=''>
+        <a href='https://www.flaticon.com/free-icons/koala' title='koala icons'>
+          Koala icons created by Xinh Studio - Flaticon
+        </a>
+      </footer>
+    </div>
+  );
+};
+export default About;
