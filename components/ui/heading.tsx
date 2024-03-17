08a60396c1f0c381442053ed7fedc747978c7a43
raslan <24810123+raslan@users.noreply.github.com>
2024-03-17 17:45:25 +0200

    feat: icon design update

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -1,13 +1,17 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import { ModeToggle } from './mode-toggle';
 import { Button } from './button';
+import AppIcon from '../icons/app';
 
 export const Heading = ({ children }: { children: string }) => {
   return (
     <div className='flex justify-between items-start'>
-      <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-7xl mb-2'>
-        {children}
-      </h1>
+      <div className='flex gap-3 items-center'>
+        <AppIcon className='w-10 h-10 lg:w-20 lg:h-20 bg-black dark:bg-transparent rounded-lg' />
+        <h1 className='mt-1 text-3xl font-extrabold tracking-tight lg:text-6xl mb-2'>
+          {children}
+        </h1>
+      </div>
       <div className='relative flex items-center gap-3 right-2 top-2'>
         <SignedOut>
           <SignInButton mode='modal'>
