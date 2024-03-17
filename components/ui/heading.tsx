3bb7cd62da01160ba6124538c04ea9fdc1b626a3
raslan <24810123+raslan@users.noreply.github.com>
2024-03-17 21:17:56 +0200

    feat: rename default header to calculator

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -6,9 +6,9 @@ import AppIcon from '../icons/app';
 export const Heading = ({ children }: { children: string }) => {
   return (
     <div className='flex justify-between items-start'>
-      <div className='flex gap-3 items-center'>
+      <div className='flex lg:gap-3 items-center'>
         <AppIcon className='w-10 h-10 lg:w-20 lg:h-20 bg-zinc-950 rounded-lg' />
-        <h1 className='mt-1 text-3xl font-extrabold tracking-tight lg:text-6xl mb-2'>
+        <h1 className='mt-2 text-2xl font-extrabold tracking-tight lg:text-6xl mb-2'>
           {children}
         </h1>
       </div>
