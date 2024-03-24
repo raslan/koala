a684939b56ef3438593c4e1ce5a6c20339017c47
raslan <24810123+raslan@users.noreply.github.com>
2024-03-24 18:28:32 +0200

    feat: use abstracted card component

--- app/settings/appearance-form.tsx
+++ app/settings/appearance-form.tsx
@@ -17,6 +17,7 @@ import {
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { toast } from 'sonner';
 import { useTheme } from 'next-themes';
+import RadioCard from '@/components/ui/radio-card';
 
 const appearanceFormSchema = z.object({
   theme: z.enum(['light', 'dark'], {
@@ -66,25 +67,7 @@ export function AppearanceForm() {
                     <FormControl>
                       <RadioGroupItem value='light' className='sr-only' />
                     </FormControl>
-                    <div className='items-center rounded-md border-2 border-muted p-1 hover:border-accent'>
-                      <div className='space-y-2 rounded-sm bg-[#ecedef] p-2'>
-                        <div className='space-y-2 rounded-md bg-white p-2 shadow-sm'>
-                          <div className='h-2 w-[80px] rounded-lg bg-[#ecedef]' />
-                          <div className='h-2 w-[100px] rounded-lg bg-[#ecedef]' />
-                        </div>
-                        <div className='flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm'>
-                          <div className='h-4 w-4 rounded-full bg-[#ecedef]' />
-                          <div className='h-2 w-[100px] rounded-lg bg-[#ecedef]' />
-                        </div>
-                        <div className='flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm'>
-                          <div className='h-4 w-4 rounded-full bg-[#ecedef]' />
-                          <div className='h-2 w-[100px] rounded-lg bg-[#ecedef]' />
-                        </div>
-                      </div>
-                    </div>
-                    <span className='block w-full p-2 text-center font-normal'>
-                      Light
-                    </span>
+                    <RadioCard>Light</RadioCard>
                   </FormLabel>
                 </FormItem>
                 <FormItem>
@@ -92,25 +75,7 @@ export function AppearanceForm() {
                     <FormControl>
                       <RadioGroupItem value='dark' className='sr-only' />
                     </FormControl>
-                    <div className='items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground'>
-                      <div className='space-y-2 rounded-sm bg-zinc-950 p-2'>
-                        <div className='space-y-2 rounded-md bg-zinc-800 p-2 shadow-sm'>
-                          <div className='h-2 w-[80px] rounded-lg bg-zinc-400' />
-                          <div className='h-2 w-[100px] rounded-lg bg-zinc-400' />
-                        </div>
-                        <div className='flex items-center space-x-2 rounded-md bg-zinc-800 p-2 shadow-sm'>
-                          <div className='h-4 w-4 rounded-full bg-zinc-400' />
-                          <div className='h-2 w-[100px] rounded-lg bg-zinc-400' />
-                        </div>
-                        <div className='flex items-center space-x-2 rounded-md bg-zinc-800 p-2 shadow-sm'>
-                          <div className='h-4 w-4 rounded-full bg-zinc-400' />
-                          <div className='h-2 w-[100px] rounded-lg bg-zinc-400' />
-                        </div>
-                      </div>
-                    </div>
-                    <span className='block w-full p-2 text-center font-normal'>
-                      Dark
-                    </span>
+                    <RadioCard dark>Dark</RadioCard>
                   </FormLabel>
                 </FormItem>
               </RadioGroup>
