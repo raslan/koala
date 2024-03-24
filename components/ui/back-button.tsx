01897961a5318b51876c2b9598d66837e9cbc9f6
raslan <24810123+raslan@users.noreply.github.com>
2024-03-24 18:29:23 +0200

    feat: create back button

---
+++ components/ui/back-button.tsx
@@ -0,0 +1,23 @@
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { buttonVariants } from './button';
+import { ArrowLeftIcon } from '@radix-ui/react-icons';
+
+const BackButton = ({ previousPage }: { previousPage: string }) => {
+  return (
+    <div className='relative -left-4 top-3 inline-flex items-center justify-center font-bold'>
+      <Link
+        className={cn(
+          buttonVariants({ variant: 'ghost' }),
+          'flex w-full h-full hover:bg-inherit font-bold gap-3'
+        )}
+        href={previousPage}
+      >
+        <ArrowLeftIcon />
+        <span>Guides</span>
+      </Link>
+    </div>
+  );
+};
+
+export default BackButton;
