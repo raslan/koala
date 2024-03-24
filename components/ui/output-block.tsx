b2a0c5cbc4a49a50861455d2e33cbacd82456da1
raslan <24810123+raslan@users.noreply.github.com>
2024-03-24 18:29:37 +0200

    feat: modify output block to allow smaller version

--- components/ui/output-block.tsx
+++ components/ui/output-block.tsx
@@ -1,22 +1,32 @@
-import { currencyNameFormatter } from '@/lib/utils';
+import { cn, currencyNameFormatter } from '@/lib/utils';
 import { TypewriterEffect } from '@/components/ui/typewriter-effect';
 
 export const OutputBlock = ({
   result,
   baseCurrency,
+  small = false,
 }: {
   result: string;
   baseCurrency: string;
+  small?: boolean;
 }) => {
   return (
     <div className='animate-in mt-2 duration-700 transform'>
       <TypewriterEffect
-        className='m-auto text-2xl'
+        className={cn(
+          'm-auto font-bold text-center',
+          small ? 'text-2xl' : 'text-base sm:text-xl md:text-3xl lg:text-5xl'
+        )}
         words={`${result}`.split(' ').map((w) => ({
           text: w,
         }))}
       />
-      <h3 className='scroll-my-10 mt-4 pb-2 text-2xl font-semibold text-primary tracking-tight transition-colors first:mt-0 text-center'>
+      <h3
+        className={cn(
+          'scroll-my-10 mt-4 pb-2 font-semibold text-primary tracking-tight transition-colors first:mt-0 text-center',
+          small ? 'text-base' : 'text-2xl'
+        )}
+      >
         in {currencyNameFormatter.of(baseCurrency)}
       </h3>
     </div>
