bec2deb9811c322df7bfa3864d22ee4c164eaf9c
raslan <24810123+raslan@users.noreply.github.com>
2024-03-24 18:28:22 +0200

    feat: use new guide card

--- app/guides/page.tsx
+++ app/guides/page.tsx
@@ -1,27 +1,6 @@
 import { Heading } from '@/components/ui/heading';
+import RadioCard from '@/components/ui/radio-card';
 import Link from 'next/link';
-
-const ArticleLink = ({ href, title }: any) => (
-  <Link className='col-span-1' href={href}>
-    <div className='items-center rounded-md border-2 border-muted p-1 hover:border-accent'>
-      <div className='space-y-2 rounded-sm bg-[#ecedef] p-2'>
-        <div className='space-y-2 rounded-md bg-white p-2 shadow-sm'>
-          <div className='h-2 w-[80px] rounded-lg bg-[#ecedef]' />
-          <div className='h-2 w-[100px] rounded-lg bg-[#ecedef]' />
-        </div>
-        <div className='flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm'>
-          <div className='h-4 w-4 rounded-full bg-[#ecedef]' />
-          <div className='h-2 w-[100px] rounded-lg bg-[#ecedef]' />
-        </div>
-        <div className='flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm'>
-          <div className='h-4 w-4 rounded-full bg-[#ecedef]' />
-          <div className='h-2 w-[100px] rounded-lg bg-[#ecedef]' />
-        </div>
-      </div>
-    </div>
-    <span className='block w-full p-2 text-center font-normal'>{title}</span>
-  </Link>
-);
 
 const guides = [
   {
@@ -34,13 +13,11 @@ const page = () => {
   return (
     <div>
       <Heading>Guides</Heading>
-      <div className='p-4 grid grid-cols-1 lg:grid-cols-3 max-w-3xl gap-4'>
+      <div className='py-6 dark:px-4 grid grid-cols-1 lg:grid-cols-3 max-w-3xl gap-4'>
         {guides.map((article) => (
-          <ArticleLink
-            href={article.href}
-            title={article.title}
-            key={article.title}
-          />
+          <Link href={article.href} key={article.title}>
+            <RadioCard dark>{article.title}</RadioCard>
+          </Link>
         ))}
       </div>
     </div>
